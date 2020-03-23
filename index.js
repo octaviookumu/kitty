98bf285e9695fda47b3895d0944763035b9c8cc3
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2020-03-23 16:57:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const lefts = document.querySelectorAll('.left');
+
+observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		// if (entry.intersectionRatio > 0) {
+		// 	entry.target.style.animation = `fadein 3s`;
+		// } else {
+		// 	entry.target.style.animation = 'none';
+		// }
+		if (!entry.isIntersecting) {
+			return;
+		}
+		entry.target.style.animation = `fadein 2s`;
+	});
+});
+
+lefts.forEach((left) => {
+	observer.observe(left);
+});
+
+const rights = document.querySelectorAll('.right');
+
+observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			return;
+		}
+		entry.target.style.animation = `fadein 3s`;
+	});
+});
+
+rights.forEach((right) => {
+	observer.observe(right);
+});
